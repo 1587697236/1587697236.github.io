265d11584224950aea93e0ef761a7bc8c13d9052
zhouyichao <1587697236@qq.com>
2022-02-14 17:43:15 +0800

    Site updated: 2022-02-14 17:43:14

--- js/Zyc.js
+++ js/Zyc.js
@@ -1,19 +1,3 @@
-/*添加图片top*/
-// var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
-/*添加到返回顶部按钮下*/
-// document.getElementById("go-up").innerHTML += top_up;
-
-
-/* 手机客户端导航栏默认隐藏 */
-// var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-// var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
-// var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
-// for (var i = 0; i < menus_item_child.length; i++) {
-//     menus_item_child[i].style.display = "none";
-//     menus_expand[i].className += " closed";
-// }
-
-
 // 可爱的Title
 var OriginTitle = document.title;
 var titleTime;
@@ -79,14 +63,3 @@ function qipao() {
 
 // 调用气泡方法
 qipao();
-
-
-// $(document).ready(function(e) {
-//     $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lete乐特');
-// });
-
-
-// bottom:
-//   - <link rel="stylesheet" href="https://butterfly.lete114.top/css/Lete.css">
-//   - <script data-pjax src="https://butterfly.lete114.top/js/Lete.js"></script>
-//   - <script src='https://unpkg.com/butterfly-friend/dist/friend.min.js'></script>
