0dc437beed34422ec2d36190db0d9845efc5a7e7
zhouyichao <1587697236@qq.com>
2022-02-14 18:38:15 +0800

    Site updated: 2022-02-14 18:38:14

--- js/Zyc.js
+++ js/Zyc.js
@@ -16,6 +16,8 @@ document.addEventListener('visibilitychange', function() {
 });
 
 
+
+
 // 气泡
 function qipao() {
     $('#page-header').circleMagic({
@@ -63,3 +65,10 @@ function qipao() {
 
 // 调用气泡方法
 qipao();
+
+
+
+
+$(document).ready(function(e) {
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lete乐特');
+});
