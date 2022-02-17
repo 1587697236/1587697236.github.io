0110c5c3e31275bfd7c180f876cfac1acdf68075
zhouyichao <1587697236@qq.com>
2022-02-17 11:46:04 +0800

    Site updated: 2022-02-17 11:46:03

--- js/Zyc.js
+++ js/Zyc.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function() {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/img/favicon.ico");
-        document.title = '(つェ⊂) 我藏好了哦~~';
+        document.title = '页面404？？？！！！';
         clearTimeout(titleTime);
     } else {
         $('[rel="icon"]').attr('href', "/img/favicon.ico");
-        document.title = '(*´∇｀*) 被你发现啦~~' + OriginTitle;
+        document.title = '(*´∇｀*) 骗你的~~' + OriginTitle;
         titleTime = setTimeout(function() {
             document.title = OriginTitle;
         }, 2000);
